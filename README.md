e444ab9c628e316c7100655eb405ee8f4383a7fd
Tobe-Wang <hzbeq@qq.com>
2025-08-05 06:27:32 +0800

    feat: 添加 Spring RabbitMQ 示例项目
    - 新增 demo-rabbitmq-web 项目，使用 Spring Boot3 和 OpenJDK 21
    - 添加 Maven 构建配置和项目依赖
    - 设置 Spring Boot 应用配置，包括开发和生产环境配置
    - 添加日志配置和 Git 相关文件- 创建项目启动类和相关文档

---
+++ README.md
@@ -0,0 +1,5 @@
+# demo-rabbitmq-web
+使用示例-Spring RabbitMQ
+> OpenJDK 21
+> 
+> Spring Boot 3
---
+++ src/main/java/cn/zhaofd/demorabbitmqweb/DemoRabbitmqWebApplication.java
@@ -0,0 +1,13 @@
+package cn.zhaofd.demorabbitmqweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRabbitmqWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRabbitmqWebApplication.class, args);
+    }
+
+}
